dbe15db2f21f3bf21136aa32f515ee4723ccc41f
linaiyuan <lny@flashforge.com>
2024-03-20 19:30:54 +0800

    add killAsyncCall function

--- src/slic3r/GUI/FlashForge/MultiComUtils.hpp
+++ src/slic3r/GUI/FlashForge/MultiComUtils.hpp
@@ -2,6 +2,7 @@
 #define slic3r_GUI_MultiComUtils_hpp_
 
 #include <functional>
+#include <memory>
 #include <string>
 #include <wx/event.h>
 #include "FlashNetworkIntfc.h"
@@ -13,6 +14,10 @@ struct ComAsyncCallFinishEvent : public wxCommandEvent {
     ComErrno ret;
 };
 wxDECLARE_EVENT(COM_ASYNC_CALL_FINISH_EVENT, ComAsyncCallFinishEvent);
+
+class ComAsyncThread;
+typedef std::shared_ptr<ComAsyncThread> com_thread_ptr_t;
+typedef std::function<ComErrno()> com_async_call_func_t;
 
 class MultiComUtils
 {
@@ -42,7 +47,9 @@ public:
 
     static ComErrno fnetRet2ComErrno(int networkRet);
 
-    static void asyncCall(wxEvtHandler *evtHandler, const std::function<ComErrno()> &func);
+    static com_thread_ptr_t asyncCall(wxEvtHandler *evtHandler, const com_async_call_func_t &func);
+
+    static void killAsyncCall(const com_thread_ptr_t &thread); // wouldn't be safe and would probably leak resources
 };
 
 }} // namespace Slic3r::GUI
